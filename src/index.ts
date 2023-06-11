a1600d76d01fa2e93538b4f3bf7786352cb6b207
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-06-11 16:30:27 +0200

    added user class and implemented get method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('online!');
+import { User } from './models/User';
+
+const user = new User({ name: 'Henrique', age: 38 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
