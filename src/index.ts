92eb820e3b2ed425fb9a178d1a4fd9985f36ddd1
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-06-11 16:47:38 +0200

    added set method to User model

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ const user = new User({ name: 'Henrique', age: 38 });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
+
+user.set({ age: 39 });
+console.log(user.get('name'));
+console.log(user.get('age'));
