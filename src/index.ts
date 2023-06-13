a76324e96dcf9b6ed85b97c7c368175a8ed1457d
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-06-13 18:26:04 +0200

    added save method to user

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 import { User } from './models/User';
-const user = new User({ id: 3 });
-user.fetch();
-
-setTimeout(() => {
-  console.log(user);
-}, 4000);
+const user = new User({ name: 'Annia', age: 40 });
+user.save();
